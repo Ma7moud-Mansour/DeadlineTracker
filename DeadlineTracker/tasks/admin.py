3d1bee479413068fcca3247d5885d1bc987aaeab
mahmoud <mahmoudabdelkareem.info@gmail.com>
2026-04-27 02:18:17 +0300

    admin panel

--- DeadlineTracker/tasks/admin.py
+++ DeadlineTracker/tasks/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import UniversityTask
 
-# Register your models here.
+
+@admin.register(UniversityTask)
+class UniversityTaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'course', 'user', 'due_date', 'is_completed', 'created_at')
+    list_filter = ('is_completed', 'user', 'course')
+    search_fields = ('title', 'course', 'user__username')
+    list_editable = ('is_completed',)
+    list_per_page = 25
+    ordering = ('-created_at',)
