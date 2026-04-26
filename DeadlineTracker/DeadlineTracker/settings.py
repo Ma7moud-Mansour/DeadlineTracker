61eedee52b3126a6db006b8d49d631b3be0b9f2a
mahmoud <mahmoudabdelkareem.info@gmail.com>
2026-04-26 22:35:59 +0300

    hosts error

--- DeadlineTracker/DeadlineTracker/settings.py
+++ DeadlineTracker/DeadlineTracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r%f%w%#3z(m=4y+67x)s7ot$ok$#j)w6e=a++^nnu*wku_-oo_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
